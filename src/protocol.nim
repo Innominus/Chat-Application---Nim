9b0d5842299b15cd5ec35a2e0ba7d1a17c804405
Innominus <kadthekad@hotmail.com>
2020-12-24 16:16:18 +1100

    first commit

---
+++ src/protocol.nim
@@ -0,0 +1,45 @@
+import json
+
+type
+    Message* = object
+        username*: string
+        message*: string
+
+proc parseMessage* (data: string): Message = 
+    let dataJson = parseJson(data)
+    result.username = dataJson["username"].getStr()
+    result.message = dataJson["message"].getStr()
+
+proc createMessage*(username, message: string): string =
+    result = $(%{
+        "username": %username,
+        "message": %message
+    }) & "\c\l"
+
+#TESTING
+when isMainModule:
+    block:
+        let data = """{"username": "John", "message": "Hi!"}"""
+        let parsed = parseMessage(data)
+        doAssert parsed.username == "John"
+        doAssert parsed.message == "Hi!"
+    
+    block:
+        let data = """foobar"""
+        
+        try:
+            let parsed = parseMessage(data)
+            doAssert false
+        except JsonParsingError:
+            doAssert true
+        except:
+            doAssert false
+    
+    block:
+        let expected = """{"username":"dom","message":"hello"}""" & "\c\l"
+        try:
+            doAssert createMessage("dom", "hello") == expected
+            echo("All tests have passed")
+
+        except:
+            echo createMessage("dom", "hello")

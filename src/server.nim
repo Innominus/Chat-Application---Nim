227e7888957fe84bcc65a4da16ea31c97d3c0e71
Innominus <67846890+Innominus@users.noreply.github.com>
2021-08-05 15:06:15 +1000

    Added documentation

--- src/server.nim
+++ src/server.nim
@@ -1,5 +1,6 @@
 import asyncdispatch, asyncnet
 
+# Creating the Client and Server objects
 type
     Client = ref object
         socket: AsyncSocket
@@ -11,13 +12,14 @@ type
         socket: AsyncSocket
         clients: seq[Client]
 
-
+# Create a new server with an empty client list
 proc newServer(): Server =
     Server(socket: newAsyncSocket(), clients: @[])
 
 proc `$`(client: Client): string =
     $client.id & "(" & client.netAddr & ")"
 
+# Processing received messages and sending them to all connected clients
 proc processMessages(server: Server, client: Client) {.async.} =
     while true:
         let line = await client.socket.recvLine()
@@ -32,10 +34,13 @@ proc processMessages(server: Server, client: Client) {.async.} =
             if c.id != client.id and c.connected:
                 await c.socket.send(line & "\c\l")
 
+# Main server loop put into a procedure
+# Takes the server and binds the address to port 7687 and then listens for requests
 proc loop(server: Server, port=7687) {.async.} = 
     server.socket.bindAddr(port.Port)
     server.socket.listen()
 
+    # On receiving a connection, adds them to the client list, and checks for messages
     while true:
         let (netAddr, clientSocket) = await server.socket.acceptAddr()
         echo("accepted connection from ", netAddr)

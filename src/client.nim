0aedce1b431b8c4dd49eab539d2ee28ea7b42d4b
Innominus <kadthekad@hotmail.com>
2021-08-05 14:41:59 +1000

    cleaned folder, moved executables to executables folder, fixed bug with typing in chat where your message would be deleted if you received a message

--- src/client.nim
+++ src/client.nim
@@ -1,6 +1,12 @@
-import os, threadpool, asyncdispatch, asyncnet, protocol, terminal
+import threadpool, asyncdispatch, asyncnet, protocol, terminal, locks, strutils
 
 
+var 
+    myLock: Lock
+    input: string = ""
+    username: string
+
+const serverAddr: string = "206.189.92.126"
 
 proc connect(socket: AsyncSocket, serverAddr: string) {.async.} =
     echo("Connecting to ", serverAddr)
@@ -10,27 +16,68 @@ proc connect(socket: AsyncSocket, serverAddr: string) {.async.} =
     while true:
         let line = await socket.recvLine()
         let parsed = parseMessage(line)
-        echo(parsed.username, " said ", parsed.message)
+        eraseLine()
+        echo("[", parsed.username, "]: ", parsed.message)
+        setCursorXPos(0)
+        withLock myLock: 
+            stdout.write(input)
         
         
+proc lineWriter(): string {.thread.} =
+    
+    {.cast(gcsafe).}:
+        
+        var 
+            ch: char = getCh()
+            msgSent: string
+
+        while true:
+            
+            case ch:
+            of {'\n', '\r', '\c'}:
+                acquire(myLock)
+                eraseLine()
+                echo("[", username, "]: ", input)
+                msgSent = input
+                input = ""
+                release(myLock)
+                return msgSent
+            of 3.char: # Ctrl + C
+                quit "Closed client"
+            of {127.char, '\b'}: # backspace
+                acquire(myLock)
+                if input.len > 0:
+                    input = input[0..^2]
+                    eraseLine()
+                    stdout.write(input)
+                    setCursorXPos(input.len)
+                release(myLock)
+            else:
+                withLock myLock: input.add ch
+                input = replace(input, "\u0000", "")
+                eraseLine()
+                stdout.write(input)
+                
+            ch = getCh()
+        
+initLock(myLock)
+
+echo("~~ENTER USERNAME~~")
+username = stdin.readLine()
 
 echo("Chat application started")
-
-#if paramCount() == 0:
-#    quit("Please specify the server address, e.g. ./client localhost")
-#let serverAddr = paramStr(1)
-const serverAddr: string = "localhost"
-
-#const serverAddr: string = "206.189.92.126"
 
 var socket = newAsyncSocket()
 asyncCheck connect(socket, serverAddr)
 
-var messageFlowVar = spawn stdin.readLine()
+setCursorXPos(0)
+var messageFlowVar = spawn lineWriter()
 while true:
     if messageFlowVar.isReady():
-        let message = createMessage("Anonymous", ^messageFlowVar)
+        let message = createMessage(username, ^messageFlowVar)
         asyncCheck socket.send(message)
-        messageFlowVar = spawn stdin.readLine()
-    
-    asyncdispatch.poll()
+        messageFlowVar = spawn lineWriter()
+    asyncdispatch.poll()       
+
+
+

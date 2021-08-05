fd2a97563394199c4d84dd23045d859b7c6967ad
Innominus <67846890+Innominus@users.noreply.github.com>
2021-08-05 14:55:36 +1000

    Update client.nim
    Added documentation

--- src/client.nim
+++ src/client.nim
@@ -1,13 +1,14 @@
 import threadpool, asyncdispatch, asyncnet, protocol, terminal, locks, strutils
 
-
+# Initialising variables
 var 
     myLock: Lock
     input: string = ""
     username: string
 
-const serverAddr: string = "206.189.92.126"
+const serverAddr: string = "ADD SERVER ADDRESS HERE"
 
+# Procedure for making a connection to the server
 proc connect(socket: AsyncSocket, serverAddr: string) {.async.} =
     echo("Connecting to ", serverAddr)
     await socket.connect(serverAddr, 7687.Port)
@@ -22,17 +23,18 @@ proc connect(socket: AsyncSocket, serverAddr: string) {.async.} =
         withLock myLock: 
             stdout.write(input)
         
-        
+# The procedure called for writing to the terminal on a separate thread        
 proc lineWriter(): string {.thread.} =
     
     {.cast(gcsafe).}:
-        
         var 
             ch: char = getCh()
             msgSent: string
-
-        while true:
             
+        # This whole while loop was necessary to rework how to type to the terminal. 
+        # Originally if you received a message, it would delete whatever you were typing because of the inate functionality of the writing to terminal function
+        # Instead, this saves whatever you type to a variable, character by character. Recreating return key functionality, and backspace functionality
+        while true:
             case ch:
             of {'\n', '\r', '\c'}:
                 acquire(myLock)
@@ -62,15 +64,19 @@ proc lineWriter(): string {.thread.} =
         
 initLock(myLock)
 
+# On startup, asks for a username to use on the server
 echo("~~ENTER USERNAME~~")
 username = stdin.readLine()
 
 echo("Chat application started")
 
+# Connecting to the server
 var socket = newAsyncSocket()
 asyncCheck connect(socket, serverAddr)
 
 setCursorXPos(0)
+
+# Spawning a new thread to the lineWriter procedure to type in your message, then sending the message to the server
 var messageFlowVar = spawn lineWriter()
 while true:
     if messageFlowVar.isReady():
